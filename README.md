91aa4e6eb2a9bad1058aa85429d2da8de5c2020f
Ravern Koh <ravernkoh@gmail.com>
2022-06-29 12:31:56 +0800

    add basic login form

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Toktik
 
 Fake social media for security workshop.
+
+## To-dos
+
+- [ ] Create quick pseudo-routing
+- [ ] Create security question page
+- [ ] Create "HACKED" page after successful entry
+- [ ] Create TokTik logo and add to page
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,14 @@
+import { Center, MantineProvider } from "@mantine/core";
 import React from "react";
 
+import LoginForm from "./LoginForm";
+
 export default function App() {
-  return <div>Hello, world!</div>;
+  return (
+    <MantineProvider>
+      <Center sx={{ height: "100vh" }}>
+        <LoginForm />
+      </Center>
+    </MantineProvider>
+  );
 }
